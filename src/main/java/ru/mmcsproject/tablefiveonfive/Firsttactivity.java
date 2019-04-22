d072643c08641db7da0ebca8817a8ca6c39565c0
Zibog <48382837+Zibog@users.noreply.github.com>
2019-04-22 10:38:49 +0300

    Add files via upload

--- src/main/java/ru/mmcsproject/tablefiveonfive/Firsttactivity.java
+++ src/main/java/ru/mmcsproject/tablefiveonfive/Firsttactivity.java
@@ -16,6 +16,8 @@ public class Firsttactivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_firsttactivity);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
